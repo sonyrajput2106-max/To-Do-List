6325f7b1c98b384d0af22565d288c7a959c5a26b
Sony Rajput <sonyrajput2106@gmail.com>
2026-07-27 18:26:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -57,7 +57,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
         toggleEmptyState();
         updateProgress();
-    }
+    }  
 
     // Add Task (Create)
     function addTask(text = null, completed = false) {
